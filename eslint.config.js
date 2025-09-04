eb6edb3fcaeb0d8949b3b2cbec1877ead8c47dea
Vladislav Tochaev <vlad.tochaev2021@gmail.com>
2025-09-04 17:43:32 +0400

    feat: set linters and alias for project

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import friendlyConfig from 'friendly-frontend-lint-config'
 
 export default defineConfig([
   globalIgnores(['dist']),
+  ...friendlyConfig,
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
